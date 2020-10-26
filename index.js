3e42bdde48865d57c69c4f60600c435322a18569
nggerber <gerberclimbs@gmail.com>
2020-10-25 19:29:53 -0600

    got sockets working

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+
+//server
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/node_modules"))
+app.use(express.static(__dirname + "/views"))
+
+//load the html onto the page
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+io.on('connection', function (socket) {
+    console.log("client conected")
+    socket.on("cool", function (data) {
+        console.log(data)
+
+    })
+    // socket.on('username', function (username) {
+    //     socket.username = username;
+    //     io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    // })
+
+    // socket.on('disconnect', function(username){
+    //     io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    // })
+
+    // socket.on('chat_message', function(message) {
+    //     io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    // });
+
+})
+
+
+
+server.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT)
+
+});
